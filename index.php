8fc5687bc5b92cea461d51938e0bc0c1ce69ad74
axelozi116 <zevakinJ2@mail.ru>
2019-02-13 22:34:00 +0300

    adsads

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<HTML>
+<BODY background="http://elitefon.ru/images/201211/elitefon.ru_24123.jpg">
+<head>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <meta charset="utf-8">
+<title>Поздравляю!</title>
+<style>
+    @keyframes example {
+        from {left: 100%;}
+        to {left: 0%;}
+    }
+   
+    .animation {
+        position: absolute;
+        width: 100%;
+        color: fuchsia;
+        font-size: 24px;
+        animation-name: example;
+        animation-duration: 4s;
+    }
+
+    .anim {
+   0% { opacity: 0; transform: translate(0px,-1000px) rotate(0deg) ;  }
+   50% { opacity: 1;  }
+   60% { transform: translate(0px,30px) rotate(0deg) ;  }
+   80% { transform: translate(0px,-10px) rotate(0deg) ;  }
+   100% { transform: translate(0px,0px) rotate(0deg) ;  }
+    }
+    .b {
+        width: 300px;
+    }
+	.a {
+		font-weight: bold; font-size: 48px; color:fuchsia;
+        text-align: center;
+	}
+    .c {
+		font-weight: bold; font-size: 36px; color:magenta;
+        text-align: center;
+    }
+    
+	.table {
+	   font-weight: bold;
+        width: 500px; /* Ширина таблицы */
+        border: 1px solid grey; /* Рамка вокруг таблицы */
+        margin: auto; /* Выравниваем таблицу по центру окна  */
+    }
+    
+</style>
+    <div>
+        <H1 class="a">Мурмурка!</H1>
+        <div style="width: 100%; height: 50px; position: relative; text-align: center">
+            <p class="animation">Дорогая моя, ты самая лучшая на свете, самая добрая и нежная, ты подарила мне любовь, которая никогда не иссякнет.C днем Святого Валентина!</p>
+        </div>
+    </div>
+    <div >
+        <table style="margin: auto;">
+            <tr>
+                <td> <img src="https://pp.userapi.com/c844416/v844416597/19e2cb/LHPRkZf3edk.jpg"; ; style="width:350px">
+                </td>
+                <td>
+                    <img src="https://pp.userapi.com/c841524/v841524966/10b39/LTDbXhHUzA0.jpg"; style="width:350px">
+                </td>
+                <td> 
+                    <img src="https://pp.userapi.com/c851020/v851020655/8259d/6EfYXjeGJAs.jpg"; style="width:350px" >
+                </td>
+            </tr>
+            <tr>
+                <td> 
+                    <img src="https://pp.userapi.com/c851420/v851420489/61ade/A14x0nj3Zlc.jpg"; style="width:350px">
+                </td>
+                <td> 
+                    <img src="https://smiles24.ru/data/smiles/blesk-serdechki-233.gif"; style="width:350px"> 
+                </td>
+                <td> 
+                    <img src="https://pp.userapi.com/c824411/v824411851/1624ab/sl7X9f4d4xc.jpg"; style="width:350px">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img src="https://pp.userapi.com/c846216/v846216379/719d4/odk_0qtZtwc.jpg"; style="width: 350px">
+                </td>
+                <td>
+                    <img src="https://pp.userapi.com/c847124/v847124597/19aa7e/cYEWVxqOhao.jpg"; style="width: 350px">
+                </td>
+                <td>
+                    <img src="https://pp.userapi.com/c630127/v630127292/28f4d/OaigbBovQ9M.jpg"; style="width: 350px">
+                </td>
+            </tr>
+        </table>
+    </div>
+</head>
+</BODY>
+</HTML>
